acb59ab510dcef4c51619fdceedc6305a96fb3fb
Todor Angelov <todor.angelov@primeholding.com>
2018-10-09 11:20:00 +0300

    feat(ethereum/private): set ports back to 9545

--- truffle.js
+++ truffle.js
@@ -23,13 +23,13 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 8545,
+            port: 9545,
             network_id: "*" // Match any network id
         },
 
         test: {
             host: "localhost",
-            port: 8545,
+            port: 9545,
             network_id: "*",
         },
         ropsten: {
